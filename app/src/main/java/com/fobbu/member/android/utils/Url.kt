fb1a56ba613e0fa38844a414df5411f3a22bd869
robin <robin@gsbitlabs.com>
2019-04-09 10:13:26 +0530

    Development URL added(9 April 2019)

--- app/src/main/java/com/fobbu/member/android/utils/Url.kt
+++ app/src/main/java/com/fobbu/member/android/utils/Url.kt
@@ -4,7 +4,9 @@ class Url
 {
     object ApiUrl
     {
-        var URL ="http://13.127.103.243:3000/"
+        /*var URL ="http://13.127.103.243:3000/"*/   // Production URL
+
+        var URL ="http://13.126.177.3:3000"  // Development URL
 
         var MAP_BASEURL: String = "https://maps.googleapis.com/maps/api/"
     }
